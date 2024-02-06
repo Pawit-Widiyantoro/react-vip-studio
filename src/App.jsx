9ec87e87abe3fd7d2f7c94e04283aca3053031a7
Pawit-Widiyantoro <pawitwidiyantoro@gmail.com>
2024-02-06 18:07:24 +0700

    2. Setup Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  
+
+  return (
+    <div className='flex justify-center bg-blue-600 text-white min-h-screen items-center'>
+      <h1>Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
