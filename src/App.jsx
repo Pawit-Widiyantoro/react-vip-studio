fe847b16014d1c425719beb94d69dc5e9f3381eb
Pawit-Widiyantoro <pawitwidiyantoro@gmail.com>
2024-02-07 22:59:57 +0700

    3. Component & Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
+import React from 'react'
+
+const Button = ({variant = 'bg-black', children = '...'}) => {
+  return (
+    <button className={`h-10 px-6 font-semibold rounded-md ${variant} children-white`} type='submit'>
+      {children}
+    </button>
+  )
+}
 
 function App() {
-  
-
   return (
     <div className='flex justify-center bg-blue-600 text-white min-h-screen items-center'>
-      <h1>Hello World!</h1>
+      <div className='flex gap-x-3'>
+        <Button variant='bg-red-700'>Login</Button>
+        <Button variant='bg-slate-700'>Register</Button>
+        <Button variant='bg-black'>Logout</Button>
+      </div>
     </div>
   )
 }
